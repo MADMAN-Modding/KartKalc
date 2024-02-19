6f875410dbd2505a0e1e45633980885ebb5dc51b
MADMAN-Modding <mad@kmdtech.net>
2024-02-18 19:02:43 -0500

    Added Main functionality

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1>Welcome to the MarioKart Match Calculator!</h1>
+<h2>Info</h2>
+<h3>You can use the calculator here: </h3>
+<p>This project is designed for 4v4 MarioKart 8 Deluxe matches. On the <a href="" target="_blank">website</a> simply select which team you are on for MarioKart and then input the points of each team.</p>
+<h2>Feedback and Pull requests are welcome!</h2>
---
+++ js/index.js
@@ -0,0 +1,95 @@
+// Point Values
+const pointValues = [15, 12, 10, 8, 6, 4, 2, 1];
+
+// Places
+const places = ["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th"];
+
+// Main function
+function main() {
+    // Resets the text
+    const placesElement = document.getElementById('places');
+    placesElement.innerHTML = "The possible places are: ";
+
+    // Gets the values from the form
+    const redPoints = +document.getElementById("redPoints").value;
+    const bluePoints = +document.getElementById("bluePoints").value;
+
+    const redTeamButton = document.getElementById("redTeamButton");
+    const blueTeamButton = document.getElementById("blueTeamButton");
+
+    const clientTeam = redTeamButton.value ? "red" : "blue";
+
+    if (clientTeam === "blue" && bluePoints > redPoints) {
+        return;
+    } else if (clientTeam === "red" && redPoints > bluePoints) {
+        return;
+    }
+
+    if (clientTeam === "blue") {
+        for (let i = 0; i < pointValues.length; i++) {
+            if (checkPointsBlue(redPoints, bluePoints, i)) {
+                placesElement.innerHTML += " " + places[i] + " | ";
+            } else {
+                for (let ii = i + 1; ii < pointValues.length; ii++) {
+                    if (checkPointsBlue(redPoints, bluePoints, i, ii)) {
+                        placesElement.innerHTML += " " + places[i] + " and " + places[ii] + " | ";
+                    } else {
+                        for (let iii = ii + 1; iii < pointValues.length; iii++) {
+                            if (checkPointsBlue(redPoints, bluePoints, i, ii, iii)) {
+                                placesElement.innerHTML += " " + places[i] + ", " + places[ii] + ", and " + places[iii] + " | ";
+                            } else {
+                                for (let iv = iii + 1; iv < pointValues.length; iv++) {
+                                    if (checkPointsBlue(redPoints, bluePoints, i, ii, iii, iv)) {
+                                        placesElement.innerHTML += " " + places[i] + ", " + places[ii] + ", " + places[iii] + ", and " + places[iv] + " | "
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    if (clientTeam === "red") {
+        for (let i = 0; i < pointValues.length; i++) {
+            if (checkPointsRed(redPoints, redPoints, i)) {
+                placesElement.innerHTML += " " + places[i] + " | ";
+            } else {
+                for (let ii = i + 1; ii < pointValues.length; ii++) {
+                    if (checkPointsRed(redPoints, redPoints, i, ii)) {
+                        placesElement.innerHTML += " " + places[i] + " and " + places[ii] + " | ";
+                    } else {
+                        for (let iii = ii + 1; iii < pointValues.length; iii++) {
+                            if (checkPointsRed(redPoints, redPoints, i, ii, iii)) {
+                                placesElement.innerHTML += " " + places[i] + ", " + places[ii] + ", and " + places[iii] + " | ";
+                            } else {
+                                for (let iv = iii + 1; iv < pointValues.length; iv++) {
+                                    if (checkPointsRed(redPoints, redPoints, i, ii, iii, iv)) {
+                                        placesElement.innerHTML += " " + places[i] + ", " + places[ii] + ", " + places[iii] + ", and " + places[iv] + " | "
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    if (placesElement.innerHTML === "The possible places are: ") {
+        placesElement.innerHTML = "No Solution :(";
+    } else {
+        placesElement.innerHTML = placesElement.innerHTML.slice(0, -3) + '.';
+    }
+}
+
+function checkPointsBlue(redPoints, bluePoints, ...indices) {
+    const totalPoints = indices.reduce((sum, index) => sum + pointValues[index], 0);
+    return bluePoints + totalPoints > redPoints;
+}
+
+function checkPointsRed(redPoints, bluePoints, ...indices) {
+    const totalPoints = indices.reduce((sum, index) => sum + pointValues[index], 0);
+    return redPoints + totalPoints > bluePoints;
+}
