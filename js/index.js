2606152dc3895f4c396a5cafb7016bda5ba620c9
MADMAN-Modding <mad@kmdtech.net>
2024-02-24 08:44:15 -0500

    Changed red and blue to yours and thiers

--- js/index.js
+++ js/index.js
@@ -23,42 +23,28 @@ function main() {
     placesElement.innerHTML = "The possible places are: ";
 
     // Gets the values from the form
-    const redPoints = +document.getElementById("redPoints").value;
-    const bluePoints = +document.getElementById("bluePoints").value;
+    const yourPoints = +document.getElementById("yourPoints").value;
+    const thierPoints = +document.getElementById("thierPoints").value;
 
-    const redTeamButton = document.getElementById("redTeamButton");
-    const blueTeamButton = document.getElementById("blueTeamButton");
-
-    // Sets the clientTeam value
-    if (redTeamButton.checked) {
-        clientTeam = "red";
-    }
-
-    if (blueTeamButton.checked) {
-        clientTeam = "blue";
-    }
-
-    if (clientTeam === "blue" && bluePoints > redPoints) {
-        return;
-    } else if (clientTeam === "red" && redPoints > bluePoints) {
+    if (yourPoints > thierPoints) {
         return;
     }
 
     // For loop that goes through the options
     for (let i = 0; i < pointValues.length; i++) {
-        if (checkPoints(redPoints, bluePoints, i)) {
+        if (checkPoints(yourPoints, thierPoints, i)) {
             placesElement.innerHTML += " " + places[i] + " " + friendlyPoints + " to " + enemyPoints + " | ";
         } else {
             for (let ii = i + 1; ii < pointValues.length; ii++) {
-                if (checkPoints(redPoints, bluePoints, i, ii)) {
+                if (checkPoints(yourPoints, thierPoints, i, ii)) {
                     placesElement.innerHTML += " " + places[i] + " and " + places[ii] + " " + friendlyPoints + " to " + enemyPoints + " | ";
                 } else {
                     for (let iii = ii + 1; iii < pointValues.length; iii++) {
-                        if (checkPoints(redPoints, bluePoints, i, ii, iii)) {
+                        if (checkPoints(yourPoints, thierPoints, i, ii, iii)) {
                             placesElement.innerHTML += " " + places[i] + ", " + places[ii] + ", and " + places[iii] + " " + friendlyPoints + " to " + enemyPoints + " | ";
                         } else {
                             for (let iv = iii + 1; iv < pointValues.length; iv++) {
-                                if (checkPoints(redPoints, bluePoints, i, ii, iii, iv)) {
+                                if (checkPoints(yourPoints, thierPoints, i, ii, iii, iv)) {
                                     placesElement.innerHTML += " " + places[i] + ", " + places[ii] + ", " + places[iii] + ", and " + places[iv] + " " + friendlyPoints + " to " + enemyPoints + " | ";
                                 }
                             }
@@ -77,30 +63,11 @@ function main() {
     }
 }
 
-function teamChange(team) {
-    if (team == 'red') {
-        document.getElementById("redPointsText").innerHTML = "<input type='number' value='" + document.getElementById("redPoints").value + "' id='redPoints'> Red Team/Your Team Points";
-        document.getElementById("bluePointsText").innerHTML = "<input type='number' value='" + document.getElementById("bluePoints").value + "' id='bluePoints'> Blue Team/Their Team Points";
-    } else if (team == 'blue') {
-        document.getElementById("redPointsText").innerHTML = "<input type='number' value='" + document.getElementById("redPoints").value + "' id='redPoints'> Red Team/Their Team Points"
-        document.getElementById("bluePointsText").innerHTML = "<input type='number' value='" + document.getElementById("bluePoints").value + "' id='bluePoints'> Blue Team/Your Team Points"
-    }
-
-}
-
 // Function for checking points
-function checkPoints(redPoints, bluePoints, ...indices) {
-    if (clientTeam == "blue") {
-        totalPoints = indices.reduce((sum, index) => sum + pointValues[index], 0);
-        remainingPoints = totalPossiblePoints - totalPoints;
-        enemyPoints = redPoints + remainingPoints;
-        friendlyPoints = bluePoints + totalPoints;
-        return bluePoints + totalPoints > redPoints + remainingPoints;
-    } else if (clientTeam == "red") {
-        totalPoints = indices.reduce((sum, index) => sum + pointValues[index], 0);
-        remainingPoints = totalPossiblePoints - totalPoints;
-        enemyPoints = bluePoints + remainingPoints;
-        friendlyPoints = redPoints + totalPoints;
-        return redPoints + totalPoints > bluePoints + remainingPoints;
-    }
+function checkPoints(yourPoints, thierPoints, ...indices) {
+    totalPoints = indices.reduce((sum, index) => sum + pointValues[index], 0);
+    remainingPoints = totalPossiblePoints - totalPoints;
+    enemyPoints = thierPoints + remainingPoints;
+    friendlyPoints = yourPoints + totalPoints;
+    return yourPoints + totalPoints > yourPoints + remainingPoints;
 }
